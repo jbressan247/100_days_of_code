a2c84d234122204e5acc8d5f534f439c313ad229
Joe <jbressan01@gmail.com>
2024-01-14 23:19:17 -0500

    day 7 complete: what a day, hangman was a challenge. feeling proud of myself for pushing through and completing this by myself (with the help of google for help on documentation) lots of troubleshooting and debugging. I feel like im getting a better grasp on for loops, while loops, and lists.

---
+++ Day 7 - Hangman.py
@@ -0,0 +1,62 @@
+from random import choice
+import hangman_words, hangman_art
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = choice(hangman_words.word_list)
+word_length = len(chosen_word)
+lives = 6
+
+print(f"the word is : {chosen_word}")
+
+# Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+
+display = []
+guessed = []
+
+for i in chosen_word:
+	display += "_"
+
+print(hangman_art.logo)
+print(hangman_art.stages[lives])
+print(f"The word is: {display}")
+
+# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# Use a while loop to let the user guess again.
+
+end_of_game = False
+
+while not end_of_game:
+	guess = input("Guess a letter: ").lower()
+
+	# Loop through each position in the chosen_word
+	# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+	# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+	if guess in guessed:
+		print("You've already guessed that letter")
+
+	for position in range(word_length):
+		letter = chosen_word[position]
+
+		if letter == guess:
+			display[position] = letter
+
+	if guess not in chosen_word and guess not in guessed:
+		lives -= 1
+		print(f"The letter {guess} is not in the word!")
+		print(hangman_art.stages[lives])
+	if guess not in guessed:
+		guessed += guess
+
+	# Print 'display' and you should see the guessed letter in
+	# the correct position and every other letter replace with "_".
+	print(f"The word is: {display}")
+	print(f"Letters guessed: {guessed}")
+
+	if lives == 0:
+		end_of_game = True
+		print("You lose!")
+
+	if "_" not in display:
+		end_of_game = True
+		print("You win!")
