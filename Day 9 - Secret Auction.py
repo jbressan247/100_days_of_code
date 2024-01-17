5850f0d68f1743d2e80aae5f57a7722a1c2aa7ca
Joe <jbressan01@gmail.com>
2024-01-16 19:40:38 -0500

    day 9 end: probably not the cleanest way to code this, will take a look and revise letter.

--- Day 9 - Secret Auction.py
+++ Day 9 - Secret Auction.py
@@ -6,22 +6,27 @@ print("Welcome to the Auction!")
 bidding = True
 bids = {}
 
+
+def find_highest_bid(bids):
+	max_bid = 0
+	winner = ""
+	for names in bids:
+		current_bid = bids[names]
+		if current_bid > max_bid:
+			max_bid = current_bid
+			winner = names
+	print("********************\n")
+	print(f"The winner is: {winner}! with a bid of: ${max_bid}\n")
+	print("********************\n")
+
+
 while bidding:
-    name = input("What is your name?: ")
-    bid = int(input("What is your bid?: $"))
-    bids[name] = bid
+	name = input("What is your name?: ")
+	bid = int(input("What is your bid?: $"))
+	bids[name] = bid
 	more_bids = input("is there other bidders? (Yes or No) ").lower()
-    if more_bids == "yes":
-        print("--------------------\n"*10)
-    else:
-        for names in bids:
-            current_bid = bids[names]
-            max_bid = 0
-            winner = ""
-            if current_bid > max_bid:
-                max_bid = current_bid
-                winner = names
-        print("********************\n")
-        print(f"The winner is: {winner}! with a bid of: ${max_bid}\n")
-        print("********************\n")
+	if more_bids == "yes":
+		print("--------------------\n" * 10)
+	else:
+		find_highest_bid(bids)
 		bidding = False
