a2a0a8aa95a0e67974c948a5ae83154f4e613ee8
Joseph Bressan <jbressan01@gmail.com>
2024-01-23 16:27:08 -0500

    lite clean up

--- Day 14 - Higher Lower/Day 14 - Higher Lower.py
+++ Day 14 - Higher Lower/Day 14 - Higher Lower.py
@@ -1,10 +1,11 @@
 from random import choice
 from high_low_data import data
 
-correct = True
+correct_times = 0
 current_choice = ""
 runner_up_choice = ""
-correct_times = 0
+
+
 def check_followers():
     global correct_times
     if current_choice["follower_count"] == runner_up_choice["follower_count"]:
@@ -19,11 +20,16 @@ def check_followers():
         print(f"{runner_up_choice["name"]} has a bigger follower count. Try again :( Streak: {correct_times}")
         return False
 
+
+correct = True
+
 random_a = choice(data)
 random_b = choice(data)
 
-print(f"Compare A: {random_a["name"]}, a {random_a["description"]}, from {random_a["country"]}: {random_a["follower_count"]}")
-print(f"Compare B: {random_b["name"]}, a {random_b["description"]}, from {random_b["country"]}: {random_b["follower_count"]}")
+print(
+    f"Compare A: {random_a["name"]}, a {random_a["description"]}, from {random_a["country"]}: {random_a["follower_count"]}")
+print(
+    f"Compare B: {random_b["name"]}, a {random_b["description"]}, from {random_b["country"]}: {random_b["follower_count"]}")
 
 while correct:
     user_choice = input("Who has more followers: 'A' or 'B'").upper()
@@ -35,10 +41,15 @@ while correct:
         current_choice = random_b
         runner_up_choice = random_a
 
+    old_choice = current_choice
+
     if correct == check_followers():
-        random_a = current_choice
+        random_a = old_choice
         random_b = choice(data)
-        print(f"Compare A: {random_a["name"]}, a {random_a["description"]}, from {random_a["country"]}: {random_a["follower_count"]}")
-        print(f"Compare B: {random_b["name"]}, a {random_b["description"]}, from {random_b["country"]}: {random_b["follower_count"]}")
+        print(
+            f"Compare A: {random_a["name"]}, a {random_a["description"]}, from {random_a["country"]}: {random_a["follower_count"]}")
+        print(
+            f"Compare B: {random_b["name"]}, a {random_b["description"]}, from {random_b["country"]}: {random_b["follower_count"]}")
     else:
         quit()
+
